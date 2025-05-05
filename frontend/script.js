ded751803458febd44e7bab4b1acb0f0563b865d
Samira <samirahekmati@gmail.com>
2025-05-05 19:32:38 +0100

    frontend initial commit

---
+++ frontend/script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("chat-form")
+const nameInput = document.getElementById("username-input");
+const messageInput = document.getElementById("message-input");
+const chatBox = document.getElementById("chat-box")
+const validation =  document.getElementById("validation")
+
+form.addEventListener("submit", function(event){
+    event.preventDefault(); // <-- IMPORTANT! Stop the form from reloading the page
+
+    const userName = nameInput.value.trim()
+    console.log("user name-->", userName)
+
+    const message = messageInput.value.trim()
+    console.log("message-->", message)
+
+    
+
+    if(userName && message) {
+        const messageElement = document.createElement("p");
+        messageElement.style.border = "1px #ccc solid"
+        messageElement.style.padding = "0.5rem"
+        messageElement.textContent = `${userName}: ${message}`;
+        chatBox.appendChild(messageElement);
+
+        messageInput.value = ""
+    }
+})
