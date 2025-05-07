e2cb9164ecc69bc9df5c6b9bf8a0d53a3672b520
Samira <samirahekmati@gmail.com>
2025-05-07 11:48:24 +0100

    add POST /messages route to handle new messages

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,11 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors()); // Allow all origins (ok for local dev)
-app.use(express.json());
+app.use(cors()); // Allow all origins (ok for local dev); so frontend can access it
+app.use(express.json()); //to parse JSON request bodies
 
+// In-memory message store
+const messages = [];
 
 // __dirname is not available by default in ES module scope.
 // Recreate __dirname in ES module
@@ -17,11 +19,29 @@ const __dirname = path.dirname(__filename);
 // Serve static files from "frontend" directory
 const frontendPath = path.join(__dirname, "..", "frontend");
 
+app.post("/messages", (req, res) =>{
+    const {userName , message} = req.body;
+    console.log("recieved post request");
+
+    if (!userName || !message) {
+        return res.status(400).json({ error: 'Username and message are required.' });
+      }
+    
+      const newMessage = { userName, message };
+      messages.push(newMessage);
+      console.log("new messages appears in the  old messages",messages)
+    
+      res.status(201).json({ success: true, message: 'Message added.' });
+    
+})
+
+
+
+
 app.get("/", (req, res, next) => {
   res.send("<h1>Backend is running.</h1>");
   console.log("Backend is running!");
 });
-
 
 
 app.use(express.static(frontendPath));
