180749a964089a68c8f816485cc964edad60018a
Samira <samirahekmati@gmail.com>
2025-05-07 10:36:59 +0100

    install cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from "express";
-import path from "path"; //used to serve static files
-import { fileURLToPath } from 'url';
+import path from "path"; // to serve static files
+import { fileURLToPath } from "url";
+import cors from "cors";
 
-const app = express()
+const app = express();
+
+app.use(cors()); // Allow all origins (ok for local dev)
+app.use(express.json());
+
 
 // __dirname is not available by default in ES module scope.
 // Recreate __dirname in ES module
@@ -10,14 +15,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from "frontend" directory
-const frontendPath = path.join(__dirname, '..', 'frontend');
+const frontendPath = path.join(__dirname, "..", "frontend");
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Backend is running.</h1>");
+  console.log("Backend is running!");
+});
+
+
+
 app.use(express.static(frontendPath));
+app.get("/index", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+  console.log("index page running");
+});
 
-app.get('/', (req,res) =>{
-    res.sendFile(path.join(frontendPath, 'index.html'))
-})
-
-
-app.listen(3000, ()=>{
-    console.log('server is listening on port 3000')
-})
+app.listen(3000, () => {
+  console.log("server is listening on port 3000");
+});
