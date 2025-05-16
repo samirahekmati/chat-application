0f519849076a222aa4709197257e03d3d08d9f9e
Samira <samirahekmati@gmail.com>
2025-05-16 13:05:10 +0100

    set up Front-end: initial commit

---
+++ websockets/frontend/script.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("chat-form");
+const nameInput = document.getElementById("username-input");
+const messageInput = document.getElementById("message-input");
+const chatBox = document.getElementById("chat-box");
