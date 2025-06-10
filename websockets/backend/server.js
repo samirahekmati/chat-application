c9a708387318330f94f73585e99f1c6cb1aa3c68
Samira <samirahekmati@gmail.com>
2025-06-10 10:16:32 +0100

    created a HTTP server listening on port 8080

--- websockets/backend/server.js
+++ websockets/backend/server.js
@@ -1,12 +1,16 @@
-// Before a WebSocket connection can be made, you have to have a HTTP web-server that can handle the HTTP upgrade handshake process.
-const http = require('http');
-const websockets = require('ws')
+// Before a WebSocket connection can be made, we have create a HTTP web-server that can handle the HTTP upgrade handshake process.
+const http = require("http");
 
-//create http server obsect
-http.createServer((req,res) =>{
-  res.write('Hello world')
+// Step 1: create a HTTP server:
+const http_server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "access-control-allow-origin": "*", // Header allows any origin to access a web resource. It's a way to bypass the same-origin policy, enabling a web page to request resources from a different domain. This header is commonly used in web development for cross-origin resource sharing (CORS) scenarios
+  });
 
-  res.end();
-}).listen(3000)
+  res.end("HTTP is running");
+});
 
-
+// Start http server listening on port 8080
+http_server.listen(8080, () => {
+  console.log("The HTTP server is listening on port 8080");
+});
