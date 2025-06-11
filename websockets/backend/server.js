1934d129ca97de83dda67e0b7af5a027da3686a7
Samira <samirahekmati@gmail.com>
2025-06-11 18:33:22 +0100

    Setup basic HTTP server to serve frontend files

--- websockets/backend/server.js
+++ websockets/backend/server.js
@@ -1,13 +1,35 @@
 // Before a WebSocket connection can be made, we have create a HTTP web-server that can handle the HTTP upgrade handshake process.
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const db = require("./db");
+
+const clients = []; // keep track of all the currently connected WebSocket clients.
 
 // Step 1: create a HTTP server:
 const http_server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "access-control-allow-origin": "*", // Header allows any origin to access a web resource. It's a way to bypass the same-origin policy, enabling a web page to request resources from a different domain. This header is commonly used in web development for cross-origin resource sharing (CORS) scenarios
-  });
+  let filePath = "./frontend" + (req.url === "/" ? "/index.html" : req.url);
+  const ext = path.extname(filePath);
+  const contentTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+  };
 
-  res.end("HTTP is running");
+  const contentType = contentTypes[ext] || "text/plain";
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": contentType,
+        "access-control-allow-origin": "*", // Header allows any origin to access a web resource. It's a way to bypass the same-origin policy, enabling a web page to request resources from a different domain. This header is commonly used in web development for cross-origin resource sharing (CORS) scenarios
+      });
+      res.end(data);
+    }
+  });
 });
 
 // Start http server listening on port 8080
@@ -16,8 +38,8 @@ http_server.listen(8080, () => {
 });
 
 // Step 2 create a Websocket server and attach it to the HTTP server
-const wbesocketServer = require('websocket').server;
+const wbesocketServer = require("websocket").server;
 const websocket = new wbesocketServer({
   httpServer: http_server,
-  autoAcceptConnections: false //you must call req.accept() manually to complete the handshake,or req.reject() to deny it.
-})
+  autoAcceptConnections: false, //you must call req.accept() manually to complete the handshake,or req.reject() to deny it.
+});
