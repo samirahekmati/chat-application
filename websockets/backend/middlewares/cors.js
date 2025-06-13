3de9d21fccb579db3ed6f4f81952589a689a8278
Samira <samirahekmati@gmail.com>
2025-06-13 13:10:41 +0100

    updated allowed origins for cors

--- websockets/backend/middlewares/cors.js
+++ websockets/backend/middlewares/cors.js
@@ -1,7 +1,10 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "*",
+  origin: [
+    "https://gco0so88wow8cwc4so4k8koc.hosting.codeyourfuture.io",
+    "http://localhost:5500" // keep for local dev
+  ],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
 };
