e6c41a637ab2a6d6fac20a192c1bedc32505d879
Samira <samirahekmati@gmail.com>
2025-06-13 13:14:54 +0100

    db path changed

--- websockets/backend/server.js
+++ websockets/backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const corsMiddleware = require("./middlewares/cors");
-const { saveMessage, getAllMessages } = require("./db");
+const { saveMessage, getAllMessages } = require("./db/db");
 const websocketServer = require("websocket").server;
 
 const app = express();
