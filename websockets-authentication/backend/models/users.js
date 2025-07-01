c8ff929312a477e043fdc23331e1032a5b7eddd0
Samira <samirahekmati@gmail.com>
2025-07-01 13:48:57 +0100

    create User model with email and password fields

---
+++ websockets-authentication/backend/models/users.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create a Mongoose model called "User" based on this schema
+const User = mongoose.model("User", userSchema);
+
+export default User;
