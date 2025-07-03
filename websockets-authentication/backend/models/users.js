840aef1457901be075ae73e5f4826f844f9d3d7c
Samira <samirahekmati@gmail.com>
2025-07-03 08:40:08 +0100

    Add username field to user schema

--- websockets-authentication/backend/models/users.js
+++ websockets-authentication/backend/models/users.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 2
+  },
   email: {
     type: String,
     required: true,
