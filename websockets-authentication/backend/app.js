be5a8b50f02c0f419e90e59c989fee1b496a3104
Samira <samirahekmati@gmail.com>
2025-07-03 19:59:28 +0100

    Create HTTP server & bind Socket.IO

--- websockets-authentication/backend/app.js
+++ websockets-authentication/backend/app.js
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import cors from "cors";
+import http from "http";
+import {Server} from "socket.io";
+
+
 
 dotenv.config();
 const app = express();
@@ -18,10 +22,36 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
 
 app.get("/test", (req, res) => {
     res.send("Server is working");
   });
+
+// Create HTTP server & bind Socket.IO
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"],
+  },
+});
+
+// Socket.IO connection handling
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+
+  // Listen for incoming chat messages from clients
+  socket.on("message", (msg) => {
+    // Broadcast received message to all connected clients
+    io.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
