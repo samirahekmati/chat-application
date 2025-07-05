21c9718613a7ecc82a5c910aaaddca297652f22c
Samira <samirahekmati@gmail.com>
2025-07-05 21:57:57 +0100

    feat: added timestamp to the ui of the messages

--- websockets-authentication/frontend/chat.js
+++ websockets-authentication/frontend/chat.js
@@ -8,18 +8,29 @@ const logoutBtn = document.getElementById("logoutBtn");
 
 const token = localStorage.getItem("token");
 const user = JSON.parse(localStorage.getItem("user"));
+console.log(user)
 
 // Redirect if not logged in
 if (!token || !user) {
   window.location.href = "index.html";
 }
 
+
 // Show a message
 function addMessage(msg, isOwn) {
   const li = document.createElement("li");
   li.textContent = `${msg.username || "User"}: ${msg.text}`;
-  if (isOwn) li.classList.add("own");
+  if (isOwn) li.classList.add("own"); // styling purposes
+
+  const timeElem = document.createElement("span");
+   timeElem.textContent = msg.timestamp
+    ? new Date(msg.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+    : "";
+  timeElem.className = "timestamp";
+
+ 
   messagesList.appendChild(li);
+  li.appendChild(timeElem)
   messagesList.scrollTop = messagesList.scrollHeight;
 }
 
@@ -29,6 +40,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     const res = await fetch(`${backendURL}/api/messages`);
     if (!res.ok) throw new Error("Failed to fetch messages");
     const messages = await res.json();
+    console.log(messages)
 
     messages.forEach(msg => {
       addMessage(msg, msg.username === user.username);
