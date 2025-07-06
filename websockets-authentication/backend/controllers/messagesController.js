44036b08d57c815b27e155688ecfbc04444891d3
Samira <samirahekmati@gmail.com>
2025-07-06 16:21:37 +0100

    feat: Add deleteMessage controller to allow users to delete their own messages

--- websockets-authentication/backend/controllers/messagesController.js
+++ websockets-authentication/backend/controllers/messagesController.js
@@ -27,3 +27,31 @@ export const createMessage = async (req, res) => {
     res.status(500).json({ message: "Failed to save message" });
   }
 };
+
+// DELETE a message by ID (only if it belongs to the user)
+export const deleteMessage = async (req, res) => {
+  const messageId = req.params.id;
+  const currentUsername = req.user?.username; // comes from verifyToken
+
+
+
+
+  try {
+    const message = await Message.findById(messageId);
+    if (!message) {
+      return res.status(404).json({ message: "Message not found" });
+    }
+    console.log("User from token:", req.user.username);
+    console.log("Message from DB:", message.username);
+
+    // Check if the message belongs to the user
+    if (message.username !== currentUsername) {
+      return res.status(403).json({ message: "You can only delete your own messages" });
+    }
+
+    await message.deleteOne();
+    res.status(200).json({ message: "Message deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete message" });
+  }
+};
