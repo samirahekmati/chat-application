e452b3f58447e669d7a46864d44464af201c9b94
Samira <samirahekmati@gmail.com>
2025-07-06 16:22:24 +0100

    feat: Add DELETE /:id route to handle message deletion with authentication

--- websockets-authentication/backend/routes/messagesRoute.js
+++ websockets-authentication/backend/routes/messagesRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAllMessages, createMessage } from "../controllers/messagesController.js";
+import { getAllMessages, createMessage, deleteMessage } from "../controllers/messagesController.js";
+import verifyToken from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getAllMessages);
 router.post("/", createMessage);
+router.delete("/:id", verifyToken, deleteMessage);
 
 export default router;
